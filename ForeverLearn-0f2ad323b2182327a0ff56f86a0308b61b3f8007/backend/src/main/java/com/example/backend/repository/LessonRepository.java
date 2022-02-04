0fb624d2621731bfc262ac27e5b90fe8aa12060a
josejustotena0@gmail.com <josejustotena0@gmail.com>
2022-02-04 16:10:12 +0100

    refresh

--- ForeverLearn-0f2ad323b2182327a0ff56f86a0308b61b3f8007/backend/src/main/java/com/example/backend/repository/LessonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonRepository extends JpaRepository<Lesson, Long> {
-}
--- ForeverLearn-0f2ad323b2182327a0ff56f86a0308b61b3f8007/backend/src/main/java/com/example/backend/repository/ThemeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Theme;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThemeRepository extends JpaRepository<Theme, Long> {
-}
